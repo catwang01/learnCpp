2df462bb8573a8f158993dd9d2d161f3cac1a074
catwang01 <edwardelricwzx@gmail.com>
2021-05-20 17:56:07 +0800

    add files

--- Linux高性能服务器编程/src/13/fork_example/fork_example.cpp
+++ Linux高性能服务器编程/src/13/fork_example/fork_example.cpp
@@ -1,6 +1,3 @@
-// This code is example code from
-// pipe(2) - Linux manual page
-// https://man7.org/linux/man-pages/man2/pipe.2.html#EXAMPLES
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -33,24 +30,23 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if (cpid == 0) // child branch
+    if (cpid == 0)
     {                     /* Child reads from pipe */
-        close(pipefd[1]); /* Child doesn't use write end, close it */
+        close(pipefd[1]); /* Close unused write end */
 
-        while (read(pipefd[0], &buf, 1) > 0) // reads a char from pipefd[0] at a time
-        {
-            write(STDOUT_FILENO, &buf, 1);  // echoes to the stdout
-        }
+        while (read(pipefd[0], &buf, 1) > 0)
+            write(STDOUT_FILENO, &buf, 1);
+
         write(STDOUT_FILENO, "\n", 1);
         close(pipefd[0]);
-        _exit(EXIT_SUCCESS); // use _exit in child process
+        _exit(EXIT_SUCCESS);
     }
-    else // parent branch
+    else
     {                     /* Parent writes argv[1] to pipe */
-        close(pipefd[0]); /* Parent doesn't use read end, close it */
+        close(pipefd[0]); /* Close unused read end */
         write(pipefd[1], argv[1], strlen(argv[1]));
         close(pipefd[1]); /* Reader will see EOF */
         wait(NULL);       /* Wait for child */
-        exit(EXIT_SUCCESS); // use exit in Parent process
+        exit(EXIT_SUCCESS);
     }
 }
